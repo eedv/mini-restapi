c8fc5590bb61dbf3346ec3baa7475f547fc60d45
Elias Vicente <evicente@admotion.local>
2019-02-01 15:30:38 -0300

    Add server listener

--- utils/app.js
+++ utils/app.js
@@ -1,13 +1,13 @@
 const app = require('express')();
 
-// app.options('*', function(req, res) {
-// 	res.header({
-// 		'Access-Control-Allow-Origin': '*',
-// 		'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
-// 		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-// 	});
-// 	res.end();
-// });
-// app.listen(process.env.PORT || 8501);
+app.options('*', function(req, res) {
+	res.header({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+	});
+	res.end();
+});
+app.listen(process.env.PORT || 8501);
 console.log('Server started')
 module.exports = app;
