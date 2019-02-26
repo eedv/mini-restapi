72af765d9374e2d3626a19b74ba79f7be7a24d26
Elias Vicente <evicente@admotion.local>
2019-02-26 11:46:04 -0300

    First aproach to CRUD orders

--- src/db-querys.js
+++ src/db-querys.js
@@ -1,4 +1,5 @@
 const db = require('./db-connection');
+const utils = require('./utils');
 const search = async (collectionName, query) => {
 	let docs = await db[collectionName].findAsCursor(
 		{$text: { $search: query }},
@@ -12,9 +13,24 @@ const getPeriod = async ({period, week}) => {
 const getAll = async (collectionName) => {
 	return await db[collectionName].find({});
 }
-
+const getOrders = async (params) => {
+	return await db.orders.find(params);
+}
+const createOrder = async () => {
+	const order = {
+		name: 'New order',
+		creationDate: new Date(),
+		year: new Date().getFullYear(),
+		period: utils.getMonth(),
+		week: utils.getWeek(),
+		products: []
+	}
+	return await db.orders.insertOne(order)
+}
 module.exports = {
 	search,
 	getPeriod,
-	getAll
+	getAll,
+	getOrders,
+	createOrder
 }
--- src/index.js
+++ src/index.js
@@ -20,3 +20,43 @@ app.get('/products', async (req, res) => {
 		res.status(404).send(err);
 	}
 });
+app.get('/orders', async (req, res) => {
+	res.header({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+	})
+	try {
+		let orders = await dbQuerys.getAll('orders');
+		res.json(orders);
+	} catch (err) {
+		console.log(err);
+		res.status(404).send(err);
+	}
+});
+app.get('/orders/:year/:period/:week', async (req, res) => {
+	res.header({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+	})
+	try {
+		_.forEach(req.params, ((value, key) => req.params[key] = Number(value)));
+		let orders = await dbQuerys.getOrders(req.params);
+		res.json(orders);
+	} catch (err) {
+		console.log(err);
+		res.status(404).send(err);
+	}
+});
+app.post('/orders/', async (req, res) => {
+	res.header({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+	})
+	try {
+		let order = await dbQuerys.createOrder();
+		res.json(order);
+	} catch (err) {
+		console.log(err);
+		res.status(404).send(err);
+	}
+});
