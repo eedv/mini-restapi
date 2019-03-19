0db7dcc99fb22d6a460f7ea2dbb8f379bb2a13d3
--set <elfoton@gmail.com>
2019-03-19 20:19:31 -0300

    Add patch to allowed methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const app = require('express')();
+var bodyParser = require('body-parser');
 
 app.options('*', function(req, res) {
 	res.header({
 		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+		'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
 		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
 	});
 	res.end();
